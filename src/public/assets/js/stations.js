ce1457e21e699a784f8d22d1fecf35e50ad94f93
AtomicZero <atomiczero@gmail.com>
2020-08-08 14:39:21 +0100

    station updated

---
+++ src/public/assets/js/stations.js
@@ -0,0 +1,42 @@
+const cardContainer = $("#stations")
+
+const renderCard = ({ stationName, stationFacilities, stationRailways, stationTrainStorage }) => {
+    const div = $("<div>").addClass("card").attr("style", "width: 18rem;")
+    const card = $(div).html(
+        ` <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${stationName}</h5>
+                    <p class="card-text">Station Facilities - ${stationFacilities}</p>
+                    <p class="card-text">Station Railways - ${stationRailways}</p>
+                    <p class="card-text">Station Train Storage - ${stationTrainStorage}</p>
+                </div>
+        `
+    )
+    cardContainer.append(card)
+}
+
+// fetch("api/station").then((stations) => {
+//     stations.forEach(renderCard)
+// })
+
+const stations = [
+    {
+        stationName: "Grand Central",
+        stationFacilities: "Good",
+        stationRailways: "Good",
+        stationTrainStorage: "Bad",
+    },
+    {
+        stationName: "Grand Central",
+        stationFacilities: "Good",
+        stationRailways: "Good",
+        stationTrainStorage: "Bad",
+    },
+    {
+        stationName: "Grand Central",
+        stationFacilities: "Good",
+        stationRailways: "Good",
+        stationTrainStorage: "Bad",
+    }
+]
+stations.forEach(renderCard)
