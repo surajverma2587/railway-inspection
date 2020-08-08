273ad4052bbe85789f1ed552c6240ff6a9885421
Suraj Verma <suraj.verma@booking.com>
2020-08-08 14:44:18 +0100

    merge frontend

--- src/public/assets/js/stations.js
+++ src/public/assets/js/stations.js
@@ -1,9 +1,14 @@
-const cardContainer = $("#stations")
+const cardContainer = $("#stations");
 
-const renderCard = ({ stationName, stationFacilities, stationRailways, stationTrainStorage }) => {
-    const div = $("<div>").addClass("card").attr("style", "width: 18rem;")
-    const card = $(div).html(
-        ` <img src="..." class="card-img-top" alt="...">
+const renderCard = ({
+  stationName,
+  stationFacilities,
+  stationRailways,
+  stationTrainStorage,
+}) => {
+  const div = $("<div>").addClass("card").attr("style", "width: 18rem;");
+  const card = $(div).html(
+    ` <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${stationName}</h5>
                     <p class="card-text">Station Facilities - ${stationFacilities}</p>
@@ -11,32 +16,32 @@ const renderCard = ({ stationName, stationFacilities, stationRailways, stationTr
                     <p class="card-text">Station Train Storage - ${stationTrainStorage}</p>
                 </div>
         `
-    )
-    cardContainer.append(card)
-}
+  );
+  cardContainer.append(card);
+};
 
 // fetch("api/station").then((stations) => {
 //     stations.forEach(renderCard)
 // })
 
 const stations = [
-    {
-        stationName: "Grand Central",
-        stationFacilities: "Good",
-        stationRailways: "Good",
-        stationTrainStorage: "Bad",
-    },
-    {
-        stationName: "Grand Central",
-        stationFacilities: "Good",
-        stationRailways: "Good",
-        stationTrainStorage: "Bad",
-    },
-    {
-        stationName: "Grand Central",
-        stationFacilities: "Good",
-        stationRailways: "Good",
-        stationTrainStorage: "Bad",
-    }
-]
-stations.forEach(renderCard)
+  {
+    stationName: "Grand Central",
+    stationFacilities: "Good",
+    stationRailways: "Good",
+    stationTrainStorage: "Bad",
+  },
+  {
+    stationName: "Grand Central",
+    stationFacilities: "Good",
+    stationRailways: "Good",
+    stationTrainStorage: "Bad",
+  },
+  {
+    stationName: "Grand Central",
+    stationFacilities: "Good",
+    stationRailways: "Good",
+    stationTrainStorage: "Bad",
+  },
+];
+stations.forEach(renderCard);
