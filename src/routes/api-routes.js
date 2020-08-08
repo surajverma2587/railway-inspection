8a40d61b3f701602ee20cd4bfe276ac2484cd3c9
Adnan <adnan_ghafoor94@hotmail.com>
2020-08-08 14:42:25 +0100

    Added Update and delete

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const db = require("../models");
+const mongoose = require("mongoose");
 
 const router = express.Router();
 
+const getAllStations = async (req, res) => {
+  const response = await db.Station.find({});
+  res.send(response);
+};
+const getStationById = async (req, res) => {
+  const id = mongoose.Schema.Types.ObjectId(req.params.id);
+  console.log(id);
+
+  const response = await db.Station.findById(id).exec();
+  console.log(response);
+  res.send(response);
+};
 const addStation = async (req, res) => {
   const {
     stationName,
@@ -28,5 +41,20 @@ const addStation = async (req, res) => {
     res.status(500).send(error.message);
   }
 };
+
+const updateStation = (req, res) => {
+  const id = req.params.id;
+  db.Station.updateOne({ _id });
+};
+const deleteStation = (req, res) => {
+  const id = req.params.id;
+  db.Station.deleteOne({ _id });
+};
+
+router.get("/station", getAllStations);
 router.post("/station", addStation);
+router.get("/station/:id", getStationById);
+router.put("/station/:id", updateStation);
+router.delete("/station/:id", deleteStation);
+
 module.exports = router;
